abd10ec9ad0a60584d14b18e4d1ade5612c86a7b
Александр Данилюк <andanilyuk@mts.ru>
2024-01-18 11:09:00 +0300

    changed migration

--- src/migrations/1672985422619-init_table.ts
+++ src/migrations/1672985422619-init_table.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class initTable1672985422619 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      "INSERT INTO public.host(title)VALUES('dev.kion.ru'),('prod.kion.ru'),('s1.dev.kion.ru'),('s2.dev.kion.ru'),('s3.dev.kion.ru'),('test.kion.ru'),('hisense.kion.ru')"
+      "INSERT INTO public.host(title)VALUES('skion.dev.kion.ru'),('skion.staging.kion.ru'),('skion.test.kion.ru'),('skion.qe.kion.ru'),('skion.kion.ru'),('hkion.dev.kion.ru'),('hkion.qe.kion.ru'),('hkion.test.kion.ru'),('hkion.kion.ru'),('hkion.staging.kion.ru'),('lkion.dev.kion.ru'),('lkion.qe.kion.ru'),('lkion.staging.kion.ru'),('lkion.test.kion.ru'),('lkion.kion.ru'),('dev.kion.ru'),('s1.dev.kion.ru'),('s2.dev.kion.ru'),('s3.dev.kion.ru'),('s1.staging.kion.ru'),('s2.staging.kion.ru'),('staging.kion.ru'),('test.kion.ru'),('s1.test.kion.ru'),('st.kion.ru'),('prod.kion.ru')"
     );
   }
 
